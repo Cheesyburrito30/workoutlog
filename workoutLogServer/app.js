f42a910d0e23e28fea9be87f5141dacd2c4c56ce
Vernon Perry <pvernon30@gmail.com>
2017-09-02 19:30:11 -0400

    added sequelize

--- workoutLogServer/app.js
+++ workoutLogServer/app.js
@@ -1,11 +1,60 @@
 const express= require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.listen(3000, function(){
+	console.log('Listening 3000')
+})
 
 app.use(require('./middleware/headers'))
 
 app.use('/api/test', function(req,res){
 	res.send ('hello world')
 })
-app.listen(3000, function(){
-	console.log('Listening 3000')
+
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('workoutlog', 'postgres', 'VlpCartel111', {
+	host:'localhost',
+	dialect: 'postgres'
+})
+
+sequelize.authenticate().then(
+	function(){
+		console.log('connected to workoutlog postgress db')
+	},
+	function(err){
+		consol.log(err)
+	}
+
+)
+//data model
+let User = sequelize.define('user', {
+	username: Sequelize.STRING,
+	passwordhash: Sequelize.STRING,
+})
+//creates table in postgres
+//
+
+User.sync()
+//User.sync({force: true}) this drops table should we need to
+app.use(bodyParser.json())   //will parse code and then turn it into JSON
+
+app.post('/api/user', function(req,res){
+	let username= req.body.user.username;
+	let pass = req.body.user.password;
+	User.create({
+		username: username,
+		passwordhash: ""
+	}).then(
+		function createSuccess(user){
+			res.json({
+				user: user,
+				message: 'create'
+			})
+		},
+		function createError(err) {
+			res.send(500, err.message)
+		}
+	)
+
 })
